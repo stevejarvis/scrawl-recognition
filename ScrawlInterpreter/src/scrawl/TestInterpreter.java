3ee1e45623bd1d7931bf10b940455a58ac3a82a5
Steve Jarvis <steve.a.jarvis@gmail.com>
2012-10-01 17:28:36 -0700

    Ignore this commit. Just changing settings.

--- ScrawlInterpreter/src/scrawl/TestInterpreter.java
+++ ScrawlInterpreter/src/scrawl/TestInterpreter.java
@@ -2,6 +2,13 @@ package scrawl;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +25,19 @@ public class TestInterpreter {
 	
 	@Test
 	public void testInstantiation() {
+		BufferedImage image = null;
+		URL location = TestInterpreter.class.getProtectionDomain().getCodeSource().getLocation();
+        System.out.println(location.getFile());
+		try {
+			image = ImageIO.read(new File(
+					"/Users/steve/Dev/scrawl/ScrawlInterpreter/test_content/9_sections/2_4_6_8_.png"));
+		} catch (IOException e) {
+			System.out.println("Failed to load test image. " +
+					"Check the relative path.");
+			e.printStackTrace();
+			assertTrue(false);
+		}
+		ImageInterpreter interp = new ImageInterpreter(image, 9);
 	}
 
 }
