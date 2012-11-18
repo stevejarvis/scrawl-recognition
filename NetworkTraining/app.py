b0dad032b625922344abbedc2455fea01f76708d
Steve Jarvis <steve.a.jarvis@gmail.com>
2012-11-18 15:16:45 -0800

    Image visualization works.
    Fixed errors in the visualization of the data and seems
    to display correctly now.

--- NetworkTraining/app.py
+++ NetworkTraining/app.py
@@ -14,13 +14,15 @@ we want to deal with a physical square.
 
 import neuralnet
 import math
+import time
+import threading
 
 '''
 The data we have currently is for 28x28 samples, possible dimensions for
 section numbers (and neural net size) are:
 4, 16, 49, or 196 total sections.
 '''
-SECTIONS = 16
+SECTIONS = 196
 
 def _two_dimension(pixels):
     ''' Pixels must be a perfect square. Access by twod[y][x] '''
@@ -58,7 +60,9 @@ def _section_contains_ink(lst, section_num, sections):
 def sections_as_ink(values, sections):
     ''' This is how we want to represent the data. Given all the pixel values,
     think of them as sections and return a list of binary values. Only represent
-    whether each section contained ink. '''
+    whether each section contained ink.
+    
+    Picture read from left to right, top to bottom, put in linear list. '''
     results = []
     for x in range(sections):
         if _section_contains_ink(values, x, sections):
@@ -69,7 +73,7 @@ def sections_as_ink(values, sections):
 
 def _learned(nn, num_sections):
     ''' Determine if the network knows what's up yet. '''
-    goal = .9
+    goal = .8
     total = 100
     correct = 0
     with open('./data/train-no-header.csv', 'r') as fh:
@@ -87,21 +91,69 @@ def _learned(nn, num_sections):
     print('%d out of %d' %(correct, total))
     return correct / total >= goal
 
-def train(nnet, num_sections):
+def _draw_things(screen, pixels, inked):
+    # Paint it black
+    screen.fill((0,0,0))
+    # Draw the pixels
+    ink = [(p,p,p) for p in pixels]
+    for y in range(28):
+        for x in range(28):
+            pygame.draw.line(screen, ink[y * 28 + x], (x, y), (x, y))
+    
+    # Draw the sections
+    sections_per_side = int(math.sqrt(SECTIONS))
+    section_dimensions = 28 / sections_per_side
+    for y in range(sections_per_side):
+        for x in range(sections_per_side):
+            color_index = y * sections_per_side + x
+            print('index: %d value: %d' %(color_index, inked[color_index]))
+            ink_color = [inked[color_index] * 255] * 3
+            start_x = section_dimensions * x +56
+            start_y = section_dimensions * y
+            pygame.draw.rect(screen, 
+                             ink_color, 
+                             (start_x, start_y, 
+                              section_dimensions, section_dimensions), 
+                             0)
+    pygame.display.flip()
+
+def train(nnet, num_sections, screen):
     ''' With chunks of data from the file, train the network. '''
     while not _learned(nnet, num_sections):
-        # This is not so good, only looking at first 100 lines every time.
         with open('./data/train-no-header.csv', 'r') as fh:
             data = []
-            for i in range(100):
-                line = fh.readline()
+            # Not at all resource friendly. If issues, use generator.
+            for line_count, line in enumerate(fh.readlines()):
+                # First character of each line is the answer digit.
                 correct_index = int(line[0])
-                ans = [1 if correct_index == i else 0 for i in range(10)]
+                ans = [1 if correct_index == i else -1 for i in range(10)]
+                # Pixels are a 0-255 color rating, comma separated, for each
+                # character in the rest of the line. 1 character -> 1 pixel.
                 pixels = [int(x) for x in line[2:].split(',')]
                 inputs = sections_as_ink(_two_dimension(pixels), num_sections)
+                if screen:
+                    t = threading.Thread(target=_draw_things,
+                                         args=(screen, pixels, inputs))
+                    t.start()
+                    # Just pause so I can decipher what this looks like.
+                    time.sleep(5)
                 data.append((inputs, ans))
-            nn.train_network(data, iters=100)
+                # We'll train on data sizes of 500
+                if line_count % 500 == 0:
+                    nn.train_network(data, momentum=0.8, iters=100)
+                    data = []
         
 if __name__ == '__main__':
-    nn = neuralnet.NeuralNetwork(SECTIONS, SECTIONS, 10)
-    train(nn, SECTIONS)
+    try:
+        import pygame
+        pygame.init()
+        screen = pygame.display.set_mode((256, 128))
+    except ImportError:
+        print('Cannot do visualizations without PyGame.')
+        screen = None
+        
+    # I read that a good number of hidden sections is the average of in & out.
+    num_hidden = int((SECTIONS + 10) / 2)
+    nn = neuralnet.NeuralNetwork(SECTIONS, num_hidden, 10)
+    
+    train(nn, SECTIONS, screen)
