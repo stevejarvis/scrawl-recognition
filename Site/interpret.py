aaef76405e27070dc9aa52118b2edd2b81e48da0
Steve A Jarvis <sjarvis@euclid.nmu.edu>
2012-12-12 23:29:06 -0500

    Should work now, trouble debugging on euclid cause I can't see logs.
    So just committing to check out on a server where I can play sudo.

--- Site/interpret.py
+++ Site/interpret.py
@@ -5,19 +5,14 @@ Website to which whatever is being drawn on should make requests.
 Input: SIZE binary bits representing ink in image, stored in 'sec'.
     1 float for each extra percentage, 5 total, for NEWST.
 
-Output: List of ranking of guesses and distance in likeliness from previous
-spot. One per line.
-E.g.:
-8 0
-3 0.12
-7 0.54
-...
+Output: json of list of tuples, guess and associated weight.
 '''
 
 from __future__ import print_function
 import cgi
 import logging
 import sys
+import json
 # This is just to get neural-net in path on my dev machine
 sys.path.append('/Users/steve/Dev/neuralnet')
 import neuralnet
@@ -26,25 +21,28 @@ import neuralnet
 SIZE = 196
 # Path to the root of the weights folder. Make sure this exists.
 WEIGHTS_PATH = r'/Users/steve/Dev/scrawl/NetworkTraining/results/%s' %SIZE
+# Because logging isn't gonna work so well in a real life web server.
+DEBUG = False
 
-# Set up the logger
-logpath = './site.log'
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename=logpath,
-                    filemode='a')
-                        
+if DEBUG:
+	# Set up the logger
+	logpath = './site.log'
+	logging.basicConfig(level=logging.INFO,
+						format='%(asctime)s %(levelname)s %(message)s',
+						datefmt='%m-%d %H:%M',
+						filename=logpath,
+						filemode='a')
+							
 # Print Header
-print('Content-type: text/html\n')
-print('<HTML>')
+print('Content-type: text/html\n\n')
 
 def find_best_weights(path):
     import os
     weights = os.listdir(path)
     if len(weights) == 0:
         print('Need to either train or fix weight path.')
-        logging.error('No weights were at the weight path.')
+		if DEBUG:
+			logging.error('No weights were at the weight path.')
         sys.exit(0)
     weights.sort()
     weights.reverse()
@@ -57,15 +55,14 @@ data = [int(x) for x in sections]
 for param in ['t','n','s','w','e']:
     data.append(float(g[param].value))
 
-logging.info('Data seems ok, got data: %s' %data)
+if DEBUG:
+	logging.info('Data seems ok, got data: %s' %data)
     
 nn = neuralnet.NeuralNetwork(SIZE + 5, SIZE + 5, 10)
 nn.load_weights(find_best_weights(WEIGHTS_PATH))
-logging.info('Weights loaded for %d network.' %SIZE)
+if DEBUG:
+	logging.info('Weights loaded for %d network.' %SIZE)
 res = nn.evaluate(list(data))
 
-''' This output will need to be expanded upon. '''
-print(res.index(max(res)))
-
-# Close up
-print('</HTML>')
+output = [(res.index(weight), weight) for weight in res]
+json.dumps(output.sort(key=lambda x: x[1]))
