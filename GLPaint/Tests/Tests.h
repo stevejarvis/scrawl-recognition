1d80d8fa19658d05461ab4fb14059ecee4c6a7d5
Steve Jarvis <steve.a.jarvis@gmail.com>
2013-02-07 21:09:01 -0500

    Need tests to find out why it sucks.

---
+++ GLPaint/Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Steve Jarvis on 2/7/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
---
+++ GLPaint/Tests/Tests.m
@@ -0,0 +1,32 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Steve Jarvis on 2/7/13.
+//
+//
+
+#import "Tests.h"
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Tests");
+}
+
+@end
