efc06e6d7d36ba6635e37fd3085869f4e8085413
Steve Jarvis <steve.a.jarvis@gmail.com>
2013-03-12 00:54:46 -0400

    Adding enormous data file because pygame required to generate.
    And pygame is not installed on the other machines. And I want
    this forever and ever.

---
+++ NetworkTraining/data/splitdata.py
@@ -0,0 +1,11 @@
+#!/opt/local/bin/python2
+
+from __future__ import print_function
+
+with open('messy-data.csv', 'r') as fh:
+    with open('test.csv', 'w') as testdata:
+        for i in range(10000):
+            print(fh.readline(), file=testdata)            
+    with open('train.csv', 'w') as traindata:
+        for line in fh.readlines():
+            print(line, file=traindata)
