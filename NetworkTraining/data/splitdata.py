725c3e7cbbccd644ad265489dea93e23d528f310
Steve Jarvis <steve.a.jarvis@gmail.com>
2013-03-16 00:38:00 -0400

    Randomize order of train and test data.

--- NetworkTraining/data/splitdata.py
+++ NetworkTraining/data/splitdata.py
@@ -1,11 +1,14 @@
 #!/opt/local/bin/python2
 
 from __future__ import print_function
+import random
 
 with open('messy-data.csv', 'r') as fh:
+    all_data = fh.readlines()
+    random.shuffle(all_data)
     with open('test.csv', 'w') as testdata:
         for i in range(50000):
-            print(fh.readline(), file=testdata, end='')            
+            print(all_data[i], file=testdata, end='')            
     with open('train.csv', 'w') as traindata:
-        for line in fh.readlines():
+        for line in all_data[50000:]:
             print(line, file=traindata, end='')
